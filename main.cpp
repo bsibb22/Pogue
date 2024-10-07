fe191165e4c718a27e10c1008ae7fd67344942d6
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-10-07 17:38:19 -0400

    (mostly) implemented tag sparser to allow for colorful and stylish console output

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "styleBank.h"
 
-
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    OutputManager OM = OutputManager();
+    //cout << OM.c_set("cyan") << "This is an example of default text." << endl;
+    //cout << "Adding " << OM.c_push("b_magenta") << "flavor" << OM.c_pop() << " text is also " << OM.s_push("i") << OM.s_push("st") << "quite straightforward" << OM.s_pop() << OM.s_pop() << "!" << endl;
+    //cout << endl;
+
+    // now the above lines of code become...
+    OM.print("[cyan]This is an example[/cyan] of default text.\n");
+    OM.print("Adding [b_magenta]flavor[/b_magenta] text is also [i]quite[/i] straightforward!?\n");
     return 0;
 }
